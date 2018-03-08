d4a6324e22d50fd92ce1d9b0d71331b4f682e542
Taylor August Giles <augustgiles8@gmail.com>
2018-03-08 04:56:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-function beatlesLoops() {
-  const[]
+function theBeatlesPlay() {
+  const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+    const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 }
+
+function 
